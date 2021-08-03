56e6006dfe8b1476045eb501f913720f5907e8c5
mariuszlas <mariusz.las@outlook.com>
2021-08-03 15:05:38 +0100

    Created an express server with a GET response to '/'.

---
+++ server/data.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const musicians = require('./data.js');
+
+// ensure corect CORS headers
+const cors = require('cors');
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.status(200).send('Welcome to the Google Clone');
+})
+
+const routes = require('./controllers/routes.js')
+
+app.use('/musicians', routes);
+
+module.exports = app;
