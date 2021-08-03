df173dc086a77ca7ecbefa54f0d9c83efc7dab0e
mariuszlas <mariusz.las@outlook.com>
2021-08-03 15:07:04 +0100

    Created index.js file into which the server is exported and can be run.

---
+++ server/index.js
@@ -0,0 +1,8 @@
+// server is imported into here and started when this file is run
+const app = require('./server');
+
+const host = 'localhost';
+const port = 3000;
+
+// start up the server
+app.listen(port, () => console.log(`Server is listening on http://${host}:${port}/`));
