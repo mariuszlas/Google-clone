3beabee86421a68305aff427a141d1b384d7e923
mariuszlas <mariusz.las@outlook.com>
2021-08-04 10:54:26 +0100

    Combined the event listeners and redirections into one function.

--- client/assets/script.js
+++ client/assets/script.js
@@ -1,38 +1,3 @@
-// on home page, when you click search load up results page with the string as a query params
-// example results.html?search=musicians
-//
-// on page load send the fetch to the api to populate the search page based on the query param
-// 2)
-
-// add extra query param for feeling lucky so result.html?search=musicians&lucky=true
-
-// when you click search button / enter the results page loads with query params
-// on the results page it loads up results from the api and displays them
-// when you click feeling lucky results page loads and returns one results
-
-
-
-
-
-function handleSubmit(e) {
-    let searchTerm = document.getElementById('searchbar').value;
-    e.preventDefault();
-    window.location = `./results.html?search=${searchTerm}`;
-}
-const form = document.querySelector('form');
-
-form.addEventListener('submit', handleSubmit);
-
-function feelingLucky() {
-    let searchTerm = document.getElementById('searchbar').value;
-    window.location = `./results.html?search=${searchTerm}&lucky=true`;
-}
-
-<<<<<<< HEAD
-let host = 'localhost';
-let port = 5000;
-
-
 function initBindings() {
 
     let luckyBbtn = document.querySelector("#lucky-btn")
@@ -41,57 +6,16 @@ function initBindings() {
     form.addEventListener('submit', e => {
         e.preventDefault();
         let value = e.target.search.value;
-        // console.log(e.target.search.value);
-        getData(value);
+        //when you click search button / enter the results page loads with query params
+        window.location = `./results.html?search=${value}&lucky=false`;   // add extra query param for feeling lucky so result.html?search=musicians&lucky=true
     })
 
     luckyBbtn.addEventListener('click', (e) => {
         e.preventDefault();
-        getLuckyData();
+        // when you click feeling lucky results page loads and returns one results
+        window.location = `./results.html?search=${value}&lucky=true`;
     })
 }
-
-
-async function getData(value) {
-
-    let url = `http://${host}:${port}/${value}`;
-    let response = await fetch(url);
-    let data = await response.json();
-    updateDOM(data);
-
-    window.location.href = './results.html'
-}
-
-
-function updateDOM(data) {
-    // create new HTML elements and add data received from server
-
-    let dataArr = data[0].results;
-    let body = document.querySelector('body');
-
-    dataArr.forEach(obj => {
-        let section = document.createElement('section');
-        let link = document.createElement('a');
-        let name = document.createElement('h3');
-        let des = document.createElement('p');
-        link.setAttribute('href', obj.content.link);
-        name.innerText = obj.content.name;
-        des.innerText = obj.content.description;
-        section.appendChild(link);
-        section.appendChild(name);
-        section.appendChild(des);
-        body.appendChild(section);
-    })
-}
-
-
-function getLuckyData() {
-    console.log('lucky data')
-}
-
 
 
 initBindings();
-=======
-
->>>>>>> cfcc24cb48282fcc18a51e88fb22629102f2d0c1
