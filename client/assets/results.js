46a5a27e3982fc4a036a46d7a28294e401dd0f33
mariuszlas <mariusz.las@outlook.com>
2021-08-04 12:48:51 +0100

    Updated comments

--- client/assets/results.js
+++ client/assets/results.js
@@ -61,35 +61,3 @@ async function getLuckyData() {
 }
 
 redirect(isLucky);
-
-// //
-// // if(urlParams.get("lucky")) {
-//     fetch(`http://localhost:5500/${searchTerm}`)
-//     .then(resp => resp.json())
-//     .then(resp => {
-//     // const results = resp[0].results
-// //     console.log(resp);
-// //     document.createElement("SECTION")
-// // })} else {
-// //     fetch(`http://localhost:5500/${searchTerm}`)
-// //     .then(resp => resp.json())
-// //     .then(resp => {
-//     const results = resp[0].results
-//     console.log("results", results)
-//     for (result of results) {
-//         console.log(result)
-//         //create a function to add to if statement
-//     const section = document.createElement("section");
-//     const header = document.createElement("h1");
-//     const name = document.createTextNode(result.content.name);
-//     const para = document.createTextNode(result.content.description);
-//     document.getElementById("returnResults").appendChild(section);
-//     section.appendChild(header).setAttribute();
-//     section.appendChild(name);
-//     section.appendChild(para);
-//
-// }
-// //
-// //
-// })
-// }
